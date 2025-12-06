65ecaa1d4d787fcec74ed6faef3dfbb8cd588aff
Astrionix <padalalmrreddy@gmail.com>
2025-12-06 19:03:34 +0530

    Secured API keys and removed config files

--- mobile/src/config/firebase.ts
+++ mobile/src/config/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 // Note: For Expo Go, we use the JS SDK entirely.
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBWksaDxzx87mlTs5VK9A2c86YY6PVrrrw", // Using Web Key for JS SDK
-    authDomain: "saferoutex-b537a.firebaseapp.com",
-    projectId: "saferoutex-b537a",
-    storageBucket: "saferoutex-b537a.firebasestorage.app",
-    messagingSenderId: "181152676678",
-    appId: "1:181152676678:web:2c56ae4acc668f30510347",
-    measurementId: "G-ZH3S606XKZ"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
